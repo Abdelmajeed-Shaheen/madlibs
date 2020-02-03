18b9e1584bbdbdf3b799609032e22452460a926e
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-02-03 14:27:20 +0200

    code added

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,20 @@
+from string import capwords
 def main():
-	# write your code here
+	time = input("Enter a number from 1 to 12:")
+	items = input("Enter a noun (plural):")
+	name = input("Enter a name:")
+	scream = input("Enter any sentence:")
+	action = input("Enter a verb:")
+
+	print("")
+	print("")
+	print("The story goes...")
+	print("")
+	print("")
+	print("It was "+time+" o\'clock when I heard a knock at the door.")
+	print("I opened the door and there was a box full of "+items+" with a note saying \"From Mr. "+capwords(name)+".\"")
+	print("Just as I closed the door I heard a scream \""+scream.upper()+"\".")
+	print("I froze in place and all I could do was "+action+" my head.")
 
 
 
